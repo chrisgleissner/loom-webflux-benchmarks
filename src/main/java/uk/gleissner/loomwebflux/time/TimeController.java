e218e154bcfe7a18c50c42189fcda837f7443fdf
Christian Gleissner <chrisgleissner@users.noreply.github.com>
2024-04-24 21:42:23 +0100

    Update TimeController.java

--- src/main/java/uk/gleissner/loomwebflux/time/TimeController.java
+++ src/main/java/uk/gleissner/loomwebflux/time/TimeController.java
@@ -16,7 +16,7 @@ public class TimeController implements LoomWebFluxController {
     private static final String API_PATH = "/epoch-millis";
     private final AppProperties appProperties;
 
-    @GetMapping({LOOM_TOMCAT + API_PATH, LOOM_NETTY + API_PATH})
+    @GetMapping({PLATFORM_TOMCAT + API_PATH, LOOM_TOMCAT + API_PATH, LOOM_NETTY + API_PATH})
     @ResponseBody
     public Long epochMillisLoomNetty(@RequestParam(required = false) Long delayInMillis) throws InterruptedException {
         Thread.sleep(actualDelay(delayInMillis, appProperties));
