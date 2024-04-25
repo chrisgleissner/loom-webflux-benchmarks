76ad7260b3acf18dd95335d4415edad2fbe7b11f
Christian Gleissner <chrisgleissner@gmail.com>
2024-04-25 09:07:49 +0100

    Support specifying approaches via CLI and GitHub actions.

--- benchmark.sh
+++ benchmark.sh
@@ -2,24 +2,30 @@
 # Performs a benchmark of Loom and WebFlux, using the scenarios defined in scenarios.csv.
 # Outputs are written to results/$scenario.
 
-approaches="platform-tomcat loom-tomcat loom-netty webflux-netty"
+approaches="loom-tomcat,loom-netty,webflux-netty"
 scenariosFile="config/scenarios.csv"
 
 function log() {
   echo "$( date +"%H:%M:%S" )" "$1"
 }
 
-while getopts 'h' opt; do
+while getopts 'ha:' opt; do
   case "$opt" in
     h)
-      echo "Usage: $( basename "$0" ) [-h] [FILE]"
+      echo "Usage: $(basename "$0") [-h] [-a APPROACHES] [FILE]"
       echo "  FILE: Scenario CSV file. Default: config/scenarios.csv"
+      echo "  -a APPROACHES: Comma-separated list of approaches. Default: loom-tomcat,loom-netty,webflux-netty"
+      echo "                 Supported approaches: platform-tomcat,loom-tomcat,loom-netty,webflux-netty"
       echo "  -h: Shows this help"
-      exit 1
+      exit 0
       ;;
-    *)
+    a)
+      approaches="$OPTARG"
+      ;;
+    \?)
       echo "Invalid option: -$OPTARG"
       exit 1
+      ;;
   esac
 done
 shift "$(( OPTIND -1 ))"
@@ -35,8 +41,11 @@ fi
 
 startSeconds=$( date +%s )
 
+echo
 log "Started Loom and WebFlux benchmark"
-log "Scenarios file: $scenariosFile"
+log "- Scenarios file: $scenariosFile"
+log "- Approaches: $approaches"
+echo
 
 ./log-system-specs.sh
 
@@ -50,8 +59,9 @@ while IFS=',' read -r scenario k6Config delayInMillis connections requestsPerSec
         continue
     fi
 
-    for approach in $approaches; do ./benchmark-scenario.sh "$approach" "$scenario" "$k6Config" "$delayInMillis" "$connections" "$requestsPerSecond" "$warmupDurationInSeconds" "$testDurationInSeconds"; done
-done < $scenariosFile
+    IFS=',' read -ra approach_array <<< "$approaches"
+    for approach in "${approach_array[@]}"; do ./benchmark-scenario.sh "$approach" "$scenario" "$k6Config" "$delayInMillis" "$connections" "$requestsPerSecond" "$warmupDurationInSeconds" "$testDurationInSeconds"; done
+done < "$scenariosFile"
 
 endSeconds=$( date +%s )
 testDurationInSeconds=$(( endSeconds - startSeconds ))
