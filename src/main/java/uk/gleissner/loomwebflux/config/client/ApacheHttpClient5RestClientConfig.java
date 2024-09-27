ce20275ca5c59d531d223d0ec9e76aef6d10a09e
Christian Gleissner <chrisgleissner@gmail.com>
2024-09-27 18:38:23 +0100

    Upgrade to Apache HttpClient 5.4 for Virtual Thread support. Improved config for Apache HttpClient. Reduced max loa for JDK HttpClient due to errors.

--- src/main/java/uk/gleissner/loomwebflux/config/client/ApacheHttpClient5RestClientConfig.java
+++ src/main/java/uk/gleissner/loomwebflux/config/client/ApacheHttpClient5RestClientConfig.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.apache.hc.client5.http.config.RequestConfig;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.core5.util.TimeValue;
 import org.apache.hc.core5.util.Timeout;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -28,14 +30,28 @@ class ApacheHttpClient5RestClientConfig {
         val baseUrl = "http://localhost:" + environment.getProperty("local.server.port");
         log.info("Create RestClient based on Apache HttpClient 5 for {}", baseUrl);
 
+        val requestConfig = RequestConfig.custom()
+            .setConnectionRequestTimeout(Timeout.of(appProperties.client().connectTimeout()))
+            .setResponseTimeout(Timeout.of(appProperties.client().responseTimeout()))
+            .build();
+
+        val maxTotalConnections = 30_000;
+        val connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(maxTotalConnections);
+        connectionManager.setDefaultMaxPerRoute(maxTotalConnections);
+
+        val httpClient = HttpClients.custom()
+            .setDefaultRequestConfig(requestConfig)
+            .setConnectionManager(connectionManager)
+            .setKeepAliveStrategy((response, context) -> TimeValue.ofSeconds(5))
+            .evictIdleConnections(TimeValue.ofSeconds(5))
+            .evictExpiredConnections()
+            .build();
+
         return RestClient.builder()
             .baseUrl(baseUrl)
-            .requestFactory(new HttpComponentsClientHttpRequestFactory(HttpClients.custom()
-                .setDefaultRequestConfig(RequestConfig.custom()
-                    .setConnectionRequestTimeout(Timeout.of(appProperties.client().connectTimeout()))
-                    .setResponseTimeout(Timeout.of(appProperties.client().responseTimeout()))
-                    .build())
-                .build()))
+            .requestFactory(new HttpComponentsClientHttpRequestFactory(httpClient))
             .build();
     }
+
 }
